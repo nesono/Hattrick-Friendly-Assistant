07f9726908ffa14a3e6830ed6a8d90d7d6b9d015
Jochen Issing <iss@ieee.org>
2010-11-02 15:04:09 +0100

    some fixes and prepard sorted select list

--- hfa_main.js
+++ hfa_main.js
@@ -1,7 +1,22 @@
-var selectboxes = document.getElementsByName('ctl00$CPMain$ddlPoolLeagues');
-selectboxes.options.length = 0;
+// search for pool counrty select box
+var selectboxes = document.getElementsByName( 'ctl00$CPMain$ddlPoolLeagues' );
 
-alert( 'First item: ' + selectboxes.options[0].text );
-/*for( iter=0; iter<selectboxes.length; iter++ )
+// sanity check
+if( selectboxes.length != 1 )
 {
-}*/
+  alert( 'selectbox search error!' );
+  return;
+}
+
+// get first and only selectbox
+var selectbox = selectboxes[0];
+// for debugging purposes
+selectbox.options.length = 0;
+
+// array for later sorting
+var sortlist = new Array(selectbox.options.length);
+
+for( iter=0; iter<selectbox.length; iter++ )
+{
+  sortlist[iter] = selectbox.options[iter];
+}
