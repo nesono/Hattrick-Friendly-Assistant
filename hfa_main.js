6be329f21a0a891b7fbf82e1338798a6f779fbe8
Jochen Issing <iss@ieee.org>
2010-11-02 15:11:09 +0100

    added remaining preparations for sorted friendly list

--- hfa_main.js
+++ hfa_main.js
@@ -1,3 +1,11 @@
+// define sort function for select list
+function sort_by_nofteams( a, b )
+{
+  // get number of teams for entry 'a'
+  // get number of teams for entry 'b'
+  // return result
+}
+
 // search for pool counrty select box
 var selectboxes = document.getElementsByName( 'ctl00$CPMain$ddlPoolLeagues' );
 
@@ -10,13 +18,21 @@ if( selectboxes.length != 1 )
 
 // get first and only selectbox
 var selectbox = selectboxes[0];
-// for debugging purposes
-selectbox.options.length = 0;
 
 // array for later sorting
 var sortlist = new Array(selectbox.options.length);
 
+// fill array with select elements
 for( iter=0; iter<selectbox.length; iter++ )
 {
   sortlist[iter] = selectbox.options[iter];
+}
+
+// sort array
+sortlist.sort(sort_by_nofteams);
+
+// apply sorting to select box
+for( iter=0; iter<selectbox.length; iter++ )
+{
+   selectbox.options[iter] = sortlist[iter];
 }
