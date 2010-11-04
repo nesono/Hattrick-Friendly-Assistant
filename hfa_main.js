78c8fc4c35a058767c65e3c116d1382a7fe781f7
Jochen Issing <iss@ieee.org>
2010-11-04 13:26:47 +0100

    added sorting - however nothing works for now :/

--- hfa_main.js
+++ hfa_main.js
@@ -2,8 +2,12 @@
 function sort_by_nofteams( a, b )
 {
   // get number of teams for entry 'a'
+  var number_a = parseInt( a.text.replace(/.*\((\d+)\)/g, "$1") );
   // get number of teams for entry 'b'
+  var number_b = parseInt( b.text.replace(/.*\((\d+)\)/g, "$1") );
   // return result
+  var result = number_a - number_b;
+  return result;
 }
 
 // search for pool counrty select box
@@ -23,17 +27,28 @@ var selectbox = selectboxes[0];
 // array for later sorting
 var sortlist = new Array(selectbox.options.length);
 
+// debug output
+var debug_text = 'Old order: ';
+
 // fill array with select elements
 for( iter=0; iter<selectbox.length; iter++ )
 {
   sortlist[iter] = selectbox.options[iter];
+  debug_text += selectbox.options[iter].text + ' ';
 }
 
 // sort array
 sortlist.sort(sort_by_nofteams);
+debug_text += '.\nNew order: ';
 
 // apply sorting to select box
 for( iter=0; iter<selectbox.length; iter++ )
 {
-   selectbox.options[iter] = sortlist[iter];
+  selectbox.options[iter] = sortlist[iter];
+  debug_text += sortlist[iter].text + ' ';
 }
+
+selectbox.options.selectedIndex = 0;
+
+//alert( debug_text )
+
