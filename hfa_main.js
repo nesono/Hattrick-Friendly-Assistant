0062103d5d7ee493e46f5c394af3b04b456ff43d
iss <iss@ieee.org>
2011-06-15 01:46:01 +0200

    several updates and fixes - waiting for test round

--- hfa_main.js
+++ hfa_main.js
@@ -1,16 +1,8 @@
-// sort function to sort by number of available teams
-function sort_by_nofteams( a, b )
-{
-  // get number of teams for entry 'a'
-  number_a = parseInt( a.text.replace(/.*\((\d+)\)/g, "$1") );
-  // get number of teams for entry 'b'
-  number_b = parseInt( b.text.replace(/.*\((\d+)\)/g, "$1") );
-  // return result
-  result = number_a - number_b;
-  return result;
-}
+// global variable to be used in sort and discard functions
+var myCountries = new Array();
 
 // sort function to sort by number of divisions per country
+// and if divisions equal by number of teams
 function sort_by_divs( a, b )
 {
   var div_a = myCountries[a.value];
@@ -48,6 +40,20 @@ function find_pool_select()
 // functionn parsing the flags page
 function flags_page()
 {
+  // check if last flag information old enough
+  var now = new Date();
+  //var last;
+  //try {
+  //  last = JSON.parse(localStorage['flags_last_update']);
+  //}
+  //catch( e ) {
+  //  last = new Date(0);
+  //}
+
+  //var diff = now - last;
+  //alert( 'diff: ' + diff );
+  // now we should check if the diff is too small to do an update?
+
   // get all flags
   var all_flags = document.getElementsByClassName( 'flag inner' );
   //alert( all_flags.length + ' flags found!' );
@@ -80,8 +86,9 @@ function flags_page()
     idx++;
   }
   //alert( visited_by.length + ' visited by' );
+
   // save data
-  localStorage['flags_visited_by'] = visited_by;
+  localStorage['flags_visited_by'] = JSON.stringify(visited_by);
 
   // get all visited countries and save them (next 127 flags)
   for( flag_idx=0; flag_idx<127; flag_idx++ )
@@ -102,11 +109,13 @@ function flags_page()
     idx++;
   }
   //alert( visited.length + ' visited' );
+
   // save data
-  localStorage['flags_visited'] = visited;
-  var now = new Date();
+  localStorage['flags_visited'] = JSON.stringify(visited);
+  // save update time for clubs flags
   localStorage['flags_last_update'] = now.toUTCString();
-  //alert( 'saved flag collection' );
+
+  //alert( 'saved flag collection at ' + localStorage['flags_last_update'] );
 
   var header1 = document.getElementsByTagName( 'h1' );
   if( header1.length > 0 )
@@ -124,6 +133,8 @@ function leagues_page()
   // get allrows from table
   var leag_rows = leages_table[0].rows;
 
+  // the resulting array
+  var divs_per_league = new Array();
   // walk through all rows
   for( r_idx=0; r_idx< leag_rows.length; r_idx++ )
   {
@@ -139,7 +150,9 @@ function leagues_page()
       var league_id = parseInt( tmp_href.substr( parse_idx+9 ) );
 
       // cell 4 contains number of divisions
-      localStorage[ 'league ' + league_id ] = leag_cols[4].innerText;
+      divs_per_league[league_id] = leag_cols[4].innerText;
+
+      //localStorage[ 'league ' + league_id ] = leag_cols[4].innerText;
 
       //alert( 'leagure ' + league_id + ' contains ' +  localStorage[ 'league ' + league_id ] + ' divisions' );
     }
@@ -148,6 +161,9 @@ function leagues_page()
       //alert( 'exception occured' );
     }
   }
+
+  // save data
+  localStorage['divs_per_league'] = JSON.stringify( divs_per_league );
 
   var header1 = document.getElementsByTagName( 'h1' );
   if( header1.length > 0 )
@@ -162,6 +178,20 @@ function challenge_page()
 {
   // get first and only selectbox
   var selectbox = find_pool_select();
+  if( selectbox == null )
+    return;
+
+  // fill my countries array with league table
+  myCountries = JSON.parse( localStorage['divs_per_league'] );
+
+  // get visited list (ignore visited by for now)
+  var visited = JSON.parse( localStorage['flags_visited'] );
+  // reset visited countries in my countries array
+  for (iter = 0; iter < visited.length; iter++ )
+    myCountries[visited[iter]] = "visited";
+
+  // get selected value from checkbox
+  var selected_value = selectbox.options[selectbox.options.selectedIndex].value;
 
   // array for later sorting
   var sortlist = new Array(selectbox.options.length);
@@ -172,30 +202,73 @@ function challenge_page()
   // remember text of selected item
   var select_text = selectbox.options[selectbox.options.selectedIndex].text;
 
+  // counts removed items
+  var rem_count=0;
+  // to store removed countries
+  var removed="";
+  // remember original length
+  var orig_len = selectbox.options.length;
+
+  // start debugging string
+  debug_text += "Found "+ selectbox.options.length + " available countries\n";
+
+  // iteration index
+  var iter = 0;
+  // go through all items in checkbox
+  while(selectbox.options.length > 0)
+  {
+    // check if country NOT already visited
+    if (myCountries[selectbox.options[0].value] != "visited")
+    {
+      // move entry to new list
+      newList[iter] = selectbox.options[0];
+      newList[iter].text += " [" + myCountries[selectbox.options[0].value] + "]";
+      iter++;
+    }
+    else
+    {
+      // remember removed text
+      removed += selectbox.options[0].text;
+      rem_count++;
+    }
+    // reset item in select box
+    selectbox.options[0] = null;
+  }
+
+  // update debug text
+  debug_text += "Kept "+ iter + " non-visited\n";
+  debug_text += "Removed "+ rem_count + " already visited\n";
+
+  // sort remaining items by divisions
+  newList.sort(sort_by_divs);
+
+  // move items back into selectbox
+  for( iter=0; iter<newList.length; iter++ )
+  {
+    selectbox.options[iter] = newList[iter];
+  }
+
+  // reset selected item
+  for( iter=0; iter<selectbox.options.length; iter++ )
+  {
+    if( selectbox[iter].value == selected_value )
+    {
+      selectbox.options.selectedIndex = iter;
+    }
+  }
+
+  // update status display
+  var textbox = document.getElementById("ctl00_ctl00_CPContent_CPMain_pnlPoolCounts");
+  // tell how many countries have been removed
+  textbox.innerHTML += "<br>Removed <b>"+ rem_count +"/" + orig_len + "</b> countries.";
+
+
   // fill array with select elements
   for( iter=0; iter<selectbox.length; iter++ )
   {
     sortlist[iter] = selectbox.options[iter];
     debug_text += selectbox.options[iter].text + ' ';
   }
-
-  // sort array
-  sortlist.sort(sort_by_nofteams);
-  debug_text += '.\nNew order: ';
-
-  var selected_index = 0;
-  // apply sorting to select box
-  for( iter=0; iter<sortlist.length; iter++ )
-  {
-    selectbox.options[iter] = sortlist[iter];
-    debug_text += sortlist[iter].text + ' ';
-
-    if( select_text == sortlist[iter].text )
-      selected_index = iter;
-  }
-
-  // recover selected item
-  selectbox.options.selectedIndex = selected_index;
 
   //alert( debug_text )
 }
